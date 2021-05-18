ee3022c4a2292e2f88cba90b8985f52743473f80
mike-bskim <mike-bskim@gmail.com>
2021-05-18 15:43:47 +0200

    2021.05.18

--- py_ad_2_4_kimbs.py
+++ py_ad_2_4_kimbs.py
@@ -1,6 +1,6 @@
 """
 Section 2
-Parallelism with Multiprocessing - multiprocessing(3) - ProcessPoolExecutor
+Parallelism with Multiprocessing - Multiprocessing(3) - ProcessPoolExecutor
 
 Keyword - ProcessPoolExecutor, as_completed, futures, timeout, dict
 
@@ -15,26 +15,6 @@ URLS =  [
         'http://www.cnn.com/',
         'https://naver.com/',
         'http://www.bbc.co.uk/',
-        'http://ruliweb.com',
-        'https://www.daum.net/',
-        'http://www.cnn.com/',
-        'https://naver.com/',
-        'http://www.bbc.co.uk/',
-        'http://ruliweb.com',
-        'https://www.daum.net/',
-        'http://www.cnn.com/',
-        'https://naver.com/',
-        'http://www.bbc.co.uk/',
-        'http://ruliweb.com',
-        'https://www.daum.net/',
-        'http://www.cnn.com/',
-        'https://naver.com/',
-        'http://www.bbc.co.uk/',
-        'http://ruliweb.com',
-        'https://www.daum.net/',
-        'http://www.cnn.com/',
-        'https://naver.com/',
-        'http://www.bbc.co.uk/',
         'http://ruliweb.com'
         ]
 # URLS = ['https://www.daum.net/',
@@ -43,35 +23,29 @@ URLS =  [
 #         'http://www.bbc.co.uk/',
 #         'http://some-made-up-domain.com/']
 
-# 실행 함수
+# 실행함수
 def load_url(url, timeout):
     with urllib.request.urlopen(url, timeout=timeout) as conn:
         return conn.read()
 
-def main():
-    # 프로세스풀 Context 영역
-    with ProcessPoolExecutor(max_workers=5) as executor:
-        # Future 로드(실행X)
-        future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
-        
-        # 중간 확인(Dict)
-        # print(future_to_url)
-        
-        # 실행
-        for future in as_completed(future_to_url): # timeout=1(테스트 추천)
-            # Key값이 Future 객체
-            url = future_to_url[future] # or future_to_url.get()
-            try:
-                # 결과
-                data = future.result()
-            except Exception as exc:
-                # 예외 처리
-                print('%r generated an exception: %s' % (url, exc))
-            else:
-                # 결과 확인
-                print('%r page is %d bytes' % (url, len(data)))
-                # print(data)
 
-# 메인 시작
+def main():
+    # 프로세스풀 context 영역
+    with ProcessPoolExecutor(max_workers=5) as executor:
+        future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
+
+    print(future_to_url)
+
+    # 실행
+    for future in as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+        except Exception as exc:
+            print('{} generated an exception: {}'.format(url, exec))
+        else:
+            print('{} page is {} bytes'.format(url, len(data)))
+
+
 if __name__ == '__main__':
-    main()
+    main()    
